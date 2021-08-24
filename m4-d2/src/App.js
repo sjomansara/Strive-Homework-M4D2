a43d255f4554a3bbbcd1933d34228aa30c1c0a4d
sjomansara <sara.sjoman@hotmail.com>
2021-08-24 15:32:35 +0200

    added footer

--- m4-d2/src/App.js
+++ m4-d2/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import MyNav from './components/MyNav'
+import MyFooter from './components/MyFooter'
 
 function App() {
   return (
     <div>
       <MyNav title="My Book Store" color="light" />
+      <MyFooter />
     </div>
   );
 }
---
+++ m4-d2/src/components/MyFooter.jsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+const MyFooter = () => (
+    <div className="footer">
+      <p>This is the footer</p>
+    </div>
+  );
+  
+  export default MyFooter;
+  
